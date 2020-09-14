858bd15debe6dcffe31fc59a481858a6fe32c8f6
Matthew R. Becker <becker.mr@gmail.com>
2020-09-13 23:25:20 -0500

    throw an error

--- repodata_tools/links.py
+++ repodata_tools/links.py
@@ -6,11 +6,11 @@ import rapidjson as json
 import requests
 
 
-# @tenacity.retry(
-#     wait=tenacity.wait_random_exponential(multiplier=1, max=60),
-#     stop=tenacity.stop_after_attempt(10),
-#     reraise=True,
-# )
+@tenacity.retry(
+    wait=tenacity.wait_random_exponential(multiplier=1, max=60),
+    stop=tenacity.stop_after_attempt(10),
+    reraise=True,
+)
 def get_latest_links():
     return json.load(io.StringIO(bz2.decompress(
         requests.get(
