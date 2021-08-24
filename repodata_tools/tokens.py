65bbd69260fd8171aa5c86c54c811a06344749a7
Matthew R. Becker <becker.mr@gmail.com>
2021-08-24 09:15:55 -0500

    ENH try this

--- repodata_tools/tokens.py
+++ repodata_tools/tokens.py
@@ -37,6 +37,7 @@ def generate_app_token(app_id, raw_pem):
         The github token. May return None if there is an error.
     """
     try:
+        print(raw_pem[0], flush=True)
         if raw_pem[0] != b'-':
             raw_pem = base64.b64decode(raw_pem)
 
