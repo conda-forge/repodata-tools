6792fa3cc9bb52cbaf6a7ac5c63539c859cab19c
Matthew R. Becker <becker.mr@gmail.com>
2021-09-06 07:26:15 -0500

    REF do not need branch here

--- repodata_tools/shards.py
+++ repodata_tools/shards.py
@@ -202,7 +202,6 @@ def push_shard(shard, shard_pth, subdir, pkg):
             "message": "added %s/%s [ci skip] [cf admin skip] ***NO_CI***" % (
                 subdir, pkg),
             "content": edata,
-            "branch": "master",
         }
 
         r = requests.put(
